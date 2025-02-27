ffc0e4e52110b26dd357aa954e784d37717649a4
Yoga <im.brindawan@gmail.com>
2025-02-27 11:07:58 +0700

    Add initial layout and page components with Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import node from '@astrojs/node';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Mengaktifkan SSR
+  output: 'server',
+
+  // Sesuaikan dengan hosting yang kamu pakai
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [alpinejs()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
